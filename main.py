f534cbb733902713058a2e62209380048d9644b9
vilma <vilma@example.com>
2025-12-04 12:00:16 -0600

    fix(routes): registrar ruta 'errores_kpi'

--- main.py
+++ main.py
@@ -98,6 +98,10 @@ def main(page: ft.Page):
             from screens.admin_respaldo import pantalla_admin_respaldo
             page.add(pantalla_admin_respaldo(page, mostrar_pantalla))
 
+        elif nombre == "errores_kpi":
+            from screens.errores_kpi import pantalla_errores_kpi
+            page.add(pantalla_errores_kpi(page, mostrar_pantalla))
+
         elif nombre == "admin_actualizaciones":
             from screens.admin_actualizaciones import pantalla_admin_actualizaciones
             page.add(pantalla_admin_actualizaciones(page, mostrar_pantalla))
